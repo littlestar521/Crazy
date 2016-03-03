e24a13bc7233f04ab5cbf72d0e5d1eff5101877a
马娟娟 <2891529590@qq.com>
2016-03-03 16:44:06 +0800

    crary

---
+++ News/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 马娟娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/News/Library/Tools/HWDefine.h
@@ -0,0 +1,22 @@
+//
+//  HWDefine.h
+//  News
+//
+//  Created by scjy on 16/3/3.
+//  Copyright © 2016年 马娟娟. All rights reserved.
+//
+
+#ifndef HWDefine_h
+#define HWDefine_h
+
+
+//首页数据接口
+#define kMainDataList @"http://dailyapi.ibaozou.com/api/v1/documents/latest"
+//分类列表接口
+#define kClassifyList @"http://dailyapi.ibaozou.com/api/v2/articles/6949159/comments"
+//音频接口
+#define k4 @"http://dailyapi.ibaozou.com/api/v1/videos/latest"
+//
+#define media @"http://dailyapi.ibaozou.com/api/v1/videos/latest?timestamp=1456970400000&"
+
+#endif /* HWDefine_h */
